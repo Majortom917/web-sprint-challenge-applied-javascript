cc44e70868eb9fd2456e50502cf41e2d3cc6cd4e
Majortom917 <70175230+Majortom917@users.noreply.github.com>
2020-10-02 08:18:39 -0700

    header

--- components/Header.js
+++ components/Header.js
@@ -10,5 +10,26 @@
 //
 // Use your function to create a header
 // and append it to the DOM inside the div.header-container
+placement = document.querySelector('.header-container')
+function Header() {
+    const head = document.createElement('div')
+    head.classList.add('header')
+    
+    const span = document.createElement('span')
+    span.classList.add('date')
+    span.textContent = 'MARCH 28, 2020'
+    head.append(span)
 
-function Header() {}
+    const h1 = document.createElement('h1')
+    h1.textContent = 'Lambda Times'
+    head.append(h1)
+
+    const span2 = document.createElement('span')
+    span2.classList.add('temp')
+    span2.textContent = '98°'
+    head.append(span2)
+
+    placement.append(head)
+    return head 
+}
+console.log(Header())
--- components/Tabs.js
+++ components/Tabs.js
@@ -1,3 +1,4 @@
+import axios from 'axios' 
 // STEP 2: Create tabs
 // -----------------------
 // Using axios send a GET request to the address: https://lambda-times-api.herokuapp.com/topics
@@ -9,3 +10,4 @@
 //    <div class="tab">topic here</div>
 //
 // NOTE: you do _not_ need to install axios as it's included in the HTML via script element
+//axios.get('https://lambda-times-api.herokuapp.com/topics').then(res =>)
